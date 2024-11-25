f5295a246fc79bbd80d4c3d54c686db209a1c5df
myrthe678105 <90244561+myrthe678105@users.noreply.github.com>
2024-11-25 14:46:58 +0100

    some mroe changes to config files. shift-service will run but has errors trying to make calls

--- shift-service/obj/Debug/net8.0/shift-service.AssemblyInfo.cs
+++ shift-service/obj/Debug/net8.0/shift-service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("shift-service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8eed02a0eed149befb29ca5e049fd945d00f08ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12c1b3ad2eb7ac626f690647467d95129e0f8f6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("shift-service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("shift-service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
