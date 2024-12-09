00a38628c50aa70f5a428b7f0fd565e49558c5b0
Betül Beril (Beth) <90333992+bberildundar@users.noreply.github.com>
2024-12-10 00:21:24 +0100

    Enable API gateway for duty-service

--- duty-service/routes/routes.go
+++ duty-service/routes/routes.go
@@ -26,18 +26,22 @@ func RegisterRoutes(serviceClient *aztables.ServiceClient) *mux.Router {
 	// middleware for all routes
 	r.Use(middlewares.GatewayHeaderMiddleware)
 
-	// duty routes
-	r.HandleFunc("/duties", dutyHandler.GetAllDuties).Methods(http.MethodGet)
-	r.HandleFunc("/duties/{PartitionKey}/{RowKey}", dutyHandler.GetDutyById).Methods(http.MethodGet) 
-	r.HandleFunc("/duties/role", dutyHandler.GetDutiesByRole).Methods(http.MethodGet)
-	r.HandleFunc("/duties", dutyHandler.CreateDuty).Methods(http.MethodPost)
-	r.HandleFunc("/duties/{PartitionKey}/{RowKey}", dutyHandler.UpdateDuty).Methods(http.MethodPut)    
-	r.HandleFunc("/duties/{PartitionKey}/{RowKey}", dutyHandler.DeleteDuty).Methods(http.MethodDelete) 
+	// group all routes under /duties
+	dutiesRouter := r.PathPrefix("/duties").Subrouter()
 
-	// duty assignment routes:
-	r.HandleFunc("/duty-assignments", dutyAssignmentHandler.GetAllDutyAssignmentsByShiftId).Methods(http.MethodGet) // example: http://localhost:5001/duty-assignments?shiftId=123e4567-e89b-12d3-a456-426614174000
-	r.HandleFunc("/duty-assignments", dutyAssignmentHandler.CreateDutyAssignments).Methods(http.MethodPost)
-	r.HandleFunc("/duty-assignments/{ShiftId}/{DutyId}", dutyAssignmentHandler.UpdateDutyAssignment).Methods(http.MethodPut)    // example: http://localhost:5001/duty-assignments/6a033746-2fa1-4267-bc86-7ff90063c97a/75a4a953-2653-4b88-a11b-a7bc01b0db9f
-	r.HandleFunc("/duty-assignments/{ShiftId}/{DutyId}", dutyAssignmentHandler.DeleteDutyAssignment).Methods(http.MethodDelete) // example: http://localhost:5001/duty-assignments/fdf07a3e-35d7-4ec5-a40a-01d9381ec4ad/56fd7686-7bef-4c58-a6ba-70e588c66a70
+	// duty routes
+	dutiesRouter.HandleFunc("", dutyHandler.GetAllDuties).Methods(http.MethodGet)
+	dutiesRouter.HandleFunc("/{PartitionKey}/{RowKey}", dutyHandler.GetDutyById).Methods(http.MethodGet)
+	dutiesRouter.HandleFunc("/role", dutyHandler.GetDutiesByRole).Methods(http.MethodGet)
+	dutiesRouter.HandleFunc("", dutyHandler.CreateDuty).Methods(http.MethodPost)
+	dutiesRouter.HandleFunc("/{PartitionKey}/{RowKey}", dutyHandler.UpdateDuty).Methods(http.MethodPut)
+	dutiesRouter.HandleFunc("/{PartitionKey}/{RowKey}", dutyHandler.DeleteDuty).Methods(http.MethodDelete)
+
+	// duty assignment routes (under /duties)
+	dutiesRouter.HandleFunc("/duty-assignments", dutyAssignmentHandler.GetAllDutyAssignmentsByShiftId).Methods(http.MethodGet)
+	dutiesRouter.HandleFunc("/duty-assignments", dutyAssignmentHandler.CreateDutyAssignments).Methods(http.MethodPost)
+	dutiesRouter.HandleFunc("/duty-assignments/{ShiftId}/{DutyId}", dutyAssignmentHandler.UpdateDutyAssignment).Methods(http.MethodPut)
+	dutiesRouter.HandleFunc("/duty-assignments/{ShiftId}/{DutyId}", dutyAssignmentHandler.DeleteDutyAssignment).Methods(http.MethodDelete)
+
 	return r
 }
