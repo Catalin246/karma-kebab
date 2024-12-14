8d47a3c42d1a4b7375caf991521e8dfbaceaf45b
myrthe678105 <90244561+myrthe678105@users.noreply.github.com>
2024-12-14 11:41:55 +0100

    ..

--- shift-service/obj/Debug/net8.0/shift-service.AssemblyInfo.cs
+++ shift-service/obj/Debug/net8.0/shift-service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("shift-service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0d7945094b1b6e887a51c75489e813653a6c302")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6db368a8a06de8607b3a30e03dde3fd92554820")]
 [assembly: System.Reflection.AssemblyProductAttribute("shift-service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("shift-service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
