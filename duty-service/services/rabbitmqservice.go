bc370ae2652ce397b742f0f06fe0b4ddf5a38cde
myrthe678105 <90244561+myrthe678105@users.noreply.github.com>
2024-12-18 11:02:35 +0100

    changed queue name and renamed shift service files

--- duty-service/services/rabbitmqservice.go
+++ duty-service/services/rabbitmqservice.go
@@ -28,12 +28,12 @@ func NewRabbitMQService(
 
 	// Declare queue
 	q, err := ch.QueueDeclare(
-		"clock-in", // queue name
-		true,             // durable
-		false,            // delete when unused
-		false,            // exclusive
-		false,            // no-wait
-		nil,              // arguments
+		"clockIn", // queue name
+		true,      // durable
+		false,     // delete when unused
+		false,     // exclusive
+		false,     // no-wait
+		nil,       // arguments
 	)
 	if err != nil {
 		log.Fatalf("Failed to declare queue: %v", err)
