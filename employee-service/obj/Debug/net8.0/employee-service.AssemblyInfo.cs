1884cbe83ee6a0c2fe26171dfd7c5f777fc22cc2
Betül Beril (Beth) <90333992+bberildundar@users.noreply.github.com>
2025-01-14 12:58:01 +0100

    Remove unused configuration and project files from .vscode and .idea

--- employee-service/obj/Debug/net8.0/employee-service.AssemblyInfo.cs
+++ employee-service/obj/Debug/net8.0/employee-service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("employee-service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98cea93a23e4cbcbb9e1801708adf6689ab604e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+666e089967305c9317263c8099f902a9c53fb404")]
 [assembly: System.Reflection.AssemblyProductAttribute("employee-service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("employee-service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
